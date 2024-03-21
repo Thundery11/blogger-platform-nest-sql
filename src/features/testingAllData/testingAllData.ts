a6c65067b07461bb0ce76cc9bd3de2d985e673ec
Illia Nizovtsov <66624311+Thundery11@users.noreply.github.com>
2024-03-21 15:54:12 +0300

    fix

--- src/features/testingAllData/testingAllData.ts
+++ src/features/testingAllData/testingAllData.ts
@@ -16,25 +16,20 @@ export class TestingAllDataController {
     @InjectDataSource() private dataSource: DataSource,
     @InjectModel(Blogs.name) private blogsModel: Model<Blogs>,
     @InjectModel(Posts.name) private postsModel: Model<Posts>,
-    @InjectModel(Users.name) private usersModel: Model<Users>,
     @InjectModel(Comments.name) private commentsModel: Model<Comments>,
     @InjectModel(LikesDbModel.name) private likesModel: Model<LikesDbModel>,
     @InjectModel(LastLikedDbModel.name)
     private lastLikedModel: Model<LastLikedDbModel>,
-    @InjectModel(SecurityDevices.name)
-    private securityDevicesModel: Model<SecurityDevices>,
   ) {}
 
   @Delete()
   @HttpCode(204)
   async deleteAll() {
-    await this.dataSource.query('TRUNCATE public."Users"');
+    await this.dataSource.query('TRUNCATE public."Users", "Devices"');
     await this.postsModel.deleteMany({});
     await this.blogsModel.deleteMany({});
-    await this.usersModel.deleteMany({});
     await this.commentsModel.deleteMany({});
     await this.lastLikedModel.deleteMany({});
     await this.likesModel.deleteMany({});
-    await this.securityDevicesModel.deleteMany({});
   }
 }
