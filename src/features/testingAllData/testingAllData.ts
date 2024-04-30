40db08416c96d2cb5807ce6103dbef2fa91c64d1
Illia Nizovtsov <66624311+Thundery11@users.noreply.github.com>
2024-04-30 18:24:38 +0300

    Update testingAllData.ts

--- src/features/testingAllData/testingAllData.ts
+++ src/features/testingAllData/testingAllData.ts
@@ -17,6 +17,7 @@ import { QuizQuestions } from '../../quizQuestions/domain/quiz-questions.entity'
 @Controller('testing/all-data')
 export class TestingAllDataController {
   constructor(
+    @InjectDataSource() private dataSource: DataSource,
     @InjectRepository(Users) private usersRepo: Repository<Users>,
     @InjectRepository(SecurityDevices)
     private securityRepo: Repository<SecurityDevices>,
@@ -34,19 +35,18 @@ export class TestingAllDataController {
   @Delete()
   @HttpCode(204)
   async deleteAll() {
-    await this.lastLikedsRepo.delete({});
-    await this.likesForCommentsRepo.delete({});
-    await this.likesForPostsRepo.delete({});
-    await this.commentsRepo.delete({});
-    await this.postsRepo.delete({});
-    await this.securityRepo.delete({});
-    await this.blogsRepo.delete({});
-    await this.usersRepo.delete({});
-    await this.qqRepo.delete({});
-    return true;
+    // await this.lastLikedsRepo.delete({});
+    // await this.likesForCommentsRepo.delete({});
+    // await this.likesForPostsRepo.delete({});
+    // await this.commentsRepo.delete({});
+    // await this.postsRepo.delete({});
+    // await this.securityRepo.delete({});
+    // await this.blogsRepo.delete({});
+    // await this.usersRepo.delete({});
+    // await this.qqRepo.delete({});
 
-    // await this.dataSource.query(
-    //   'TRUNCATE public."users", public."devices", public."blogs", public."posts", public."comments", public."likesForComments", public."likesForPosts", public."lastLiked" CASCADE',
-    // );
+    return await this.dataSource.query(
+      'TRUNCATE public."users", public."security_devices", public."blogs", public."posts", public."comments", public."likes_for_comments", public."likes_for_posts", public."last_liked", public."quiz_questions" CASCADE',
+    );
   }
 }
