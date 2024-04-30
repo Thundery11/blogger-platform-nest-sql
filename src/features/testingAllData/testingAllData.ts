c0fe03336797d05b95b017daba1916ffacde43e2
Illia Nizovtsov <66624311+Thundery11@users.noreply.github.com>
2024-04-30 18:43:08 +0300

    Update testingAllData.ts

--- src/features/testingAllData/testingAllData.ts
+++ src/features/testingAllData/testingAllData.ts
@@ -35,15 +35,15 @@ export class TestingAllDataController {
   @Delete()
   @HttpCode(204)
   async deleteAll() {
-    // await this.lastLikedsRepo.delete({});
-    // await this.likesForCommentsRepo.delete({});
-    // await this.likesForPostsRepo.delete({});
-    // await this.commentsRepo.delete({});
-    // await this.postsRepo.delete({});
-    // await this.securityRepo.delete({});
-    // await this.blogsRepo.delete({});
-    // await this.usersRepo.delete({});
-    // await this.qqRepo.delete({});
+    await this.lastLikedsRepo.delete({});
+    await this.likesForCommentsRepo.delete({});
+    await this.likesForPostsRepo.delete({});
+    await this.commentsRepo.delete({});
+    await this.postsRepo.delete({});
+    await this.securityRepo.delete({});
+    await this.blogsRepo.delete({});
+    await this.usersRepo.delete({});
+    await this.qqRepo.delete({});
     return true;
   }
 
