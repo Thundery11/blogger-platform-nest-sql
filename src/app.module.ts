b42ceae3f0390388a0e26c46a2d49455fb41de55
Illia Nizovtsov <66624311+Thundery11@users.noreply.github.com>
2024-05-23 21:13:23 +0300

    fififfi

--- src/app.module.ts
+++ src/app.module.ts
@@ -162,7 +162,7 @@ export const localDbOptions: TypeOrmModuleOptions = {
     AuthModule,
     UsersModule,
     CqrsModule,
-    TypeOrmModule.forRoot(localDbOptions),
+    TypeOrmModule.forRoot(options),
     TypeOrmModule.forFeature([
       Blogs,
       Posts,
