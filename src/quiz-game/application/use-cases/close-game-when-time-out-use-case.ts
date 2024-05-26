94e9fe27fc1a3b1523800f9658c003696a246a0f
Illia Nizovtsov <66624311+Thundery11@users.noreply.github.com>
2024-05-26 12:43:26 +0300

    Update close-game-when-time-out-use-case.ts

--- src/quiz-game/application/use-cases/close-game-when-time-out-use-case.ts
+++ src/quiz-game/application/use-cases/close-game-when-time-out-use-case.ts
@@ -24,10 +24,9 @@ export class CloseGameWhenTimeOutUseCase
     console.log('🚀 ~ execute ~ tenSecondsAgo:', tenSecondsAgo);
 
     const processGame = async (game) => {
-      const { firstPlayerProgress, secondPlayerProgress } = game;
-      const gameId = game.id;
+      const { id: gameId, firstPlayerProgress, secondPlayerProgress } = game;
       const firstPlayerId = firstPlayerProgress.player.id;
-      const secondPlayerId = firstPlayerProgress.player.id;
+      const secondPlayerId = secondPlayerProgress.player.id;
       const finalScorePoint = 1;
 
       const shouldEndGame = async (progress, otherProgress, progressId) => {
@@ -36,11 +35,11 @@ export class CloseGameWhenTimeOutUseCase
           progress.answers.length === 5 &&
           otherProgress.answers.length !== 5
         ) {
-          const latestAddedAt = progress.answers.reduce((latest, current) => {
-            return new Date(latest.addedAt) > new Date(current.addedAt)
+          const latestAddedAt = progress.answers.reduce((latest, current) =>
+            new Date(latest.addedAt) > new Date(current.addedAt)
               ? latest
-              : current;
-          }).addedAt;
+              : current,
+          ).addedAt;
 
           if (new Date(latestAddedAt).toISOString() < tenSecondsAgo) {
             console.log('🚀 ~ shouldEndGame ~ latestAddedAt:', finalScorePoint);
@@ -48,61 +47,154 @@ export class CloseGameWhenTimeOutUseCase
               '🚀 ~ shouldEndGame ~ otherProgress ID:',
               otherProgress.id,
             );
+
             const res = await this.quizRepository.addPlayerScoreToDb(
               progressId,
               finalScorePoint,
             );
             console.log('🚀 ~ shouldEndGame ~ res:', res);
+
             await this.quizRepository.endTheGame(date.toISOString(), gameId);
             await this.quizRepository.finishPlayerProgress(progress.id);
             await this.quizRepository.finishPlayerProgress(otherProgress.id);
-            const statsForFirstPlayer =
-              await this.quizQueryRepository.getTotalScore(firstPlayerId);
-            console.log(
-              '🚀 ~ shouldEndGame ~ statsForFirstPlayer:',
-              statsForFirstPlayer,
-            );
 
-            const statsForSecondPlayer =
-              await this.quizQueryRepository.getTotalScore(secondPlayerId);
-            console.log(
-              '🚀 ~ shouldEndGame ~ statsForSecondPlayer:',
-              statsForSecondPlayer,
-            );
-
-            await this.quizRepository.setStatistcsOfUsers(
-              firstPlayerId,
-              statsForFirstPlayer,
-            );
-            await this.quizRepository.setStatistcsOfUsers(
-              secondPlayerId,
-              statsForSecondPlayer,
-            );
             return true;
           }
-          console.log('🚀 ~ shouldEndGame ~ finalScorePoint:', finalScorePoint);
-          console.log('🚀 ~ shouldEndGame ~ progressId:', progressId);
-          console.log('🚀 ~ shouldEndGame ~ progressId:', progressId);
-          console.log('🚀 ~ shouldEndGame ~ progressId:', progressId);
         }
-        console.log('🚀 ~ shouldEndGame ~ progressId:', progressId);
-
         return false;
       };
 
-      await shouldEndGame(
-        firstPlayerProgress,
-        secondPlayerProgress,
-        firstPlayerProgress.id,
-      );
-      await shouldEndGame(
-        secondPlayerProgress,
-        firstPlayerProgress,
-        secondPlayerProgress.id,
-      );
+      const gameEnded =
+        (await shouldEndGame(
+          firstPlayerProgress,
+          secondPlayerProgress,
+          firstPlayerProgress.id,
+        )) ||
+        (await shouldEndGame(
+          secondPlayerProgress,
+          firstPlayerProgress,
+          secondPlayerProgress.id,
+        ));
+
+      if (gameEnded) {
+        const statsForFirstPlayer =
+          await this.quizQueryRepository.getTotalScore(firstPlayerId);
+        console.log(
+          '🚀 ~ processGame ~ statsForFirstPlayer:',
+          statsForFirstPlayer,
+        );
+
+        const statsForSecondPlayer =
+          await this.quizQueryRepository.getTotalScore(secondPlayerId);
+        console.log(
+          '🚀 ~ processGame ~ statsForSecondPlayer:',
+          statsForSecondPlayer,
+        );
+
+        await this.quizRepository.setStatistcsOfUsers(
+          firstPlayerId,
+          statsForFirstPlayer,
+        );
+        await this.quizRepository.setStatistcsOfUsers(
+          secondPlayerId,
+          statsForSecondPlayer,
+        );
+      }
     };
 
     await Promise.all(games!.map(processGame));
     return true;
   }
+
+  // // @Cron(CronExpression.EVERY_SECOND)
+  // async execute(command: CloseGameWhenTimeOutCommand) {
+  //   const games = await this.quizQueryRepository.findNotFinishedGames();
+  //   console.log('🚀 ~ execute ~ games:', games);
+
+  //   const date = new Date();
+  //   const tenSecondsAgo = new Date(date.getTime() - 9000).toISOString();
+  //   console.log('🚀 ~ execute ~ tenSecondsAgo:', tenSecondsAgo);
+
+  //   const processGame = async (game) => {
+  //     const { firstPlayerProgress, secondPlayerProgress } = game;
+  //     const gameId = game.id;
+  //     const firstPlayerId = firstPlayerProgress.player.id;
+  //     const secondPlayerId = firstPlayerProgress.player.id;
+  //     const finalScorePoint = 1;
+
+  //     const shouldEndGame = async (progress, otherProgress, progressId) => {
+  //       console.log('🚀 ~ shouldEndGame ~ progressId:', progressId);
+  //       if (
+  //         progress.answers.length === 5 &&
+  //         otherProgress.answers.length !== 5
+  //       ) {
+  //         const latestAddedAt = progress.answers.reduce((latest, current) => {
+  //           return new Date(latest.addedAt) > new Date(current.addedAt)
+  //             ? latest
+  //             : current;
+  //         }).addedAt;
+
+  //         if (new Date(latestAddedAt).toISOString() < tenSecondsAgo) {
+  //           console.log('🚀 ~ shouldEndGame ~ latestAddedAt:', finalScorePoint);
+  //           console.log(
+  //             '🚀 ~ shouldEndGame ~ otherProgress ID:',
+  //             otherProgress.id,
+  //           );
+  //           const res = await this.quizRepository.addPlayerScoreToDb(
+  //             progressId,
+  //             finalScorePoint,
+  //           );
+  //           console.log('🚀 ~ shouldEndGame ~ res:', res);
+  //           await this.quizRepository.endTheGame(date.toISOString(), gameId);
+  //           await this.quizRepository.finishPlayerProgress(progress.id);
+  //           await this.quizRepository.finishPlayerProgress(otherProgress.id);
+  //           const statsForFirstPlayer =
+  //             await this.quizQueryRepository.getTotalScore(firstPlayerId);
+  //           console.log(
+  //             '🚀 ~ shouldEndGame ~ statsForFirstPlayer:',
+  //             statsForFirstPlayer,
+  //           );
+
+  //           const statsForSecondPlayer =
+  //             await this.quizQueryRepository.getTotalScore(secondPlayerId);
+  //           console.log(
+  //             '🚀 ~ shouldEndGame ~ statsForSecondPlayer:',
+  //             statsForSecondPlayer,
+  //           );
+
+  //           await this.quizRepository.setStatistcsOfUsers(
+  //             firstPlayerId,
+  //             statsForFirstPlayer,
+  //           );
+  //           await this.quizRepository.setStatistcsOfUsers(
+  //             secondPlayerId,
+  //             statsForSecondPlayer,
+  //           );
+  //           return true;
+  //         }
+  //         console.log('🚀 ~ shouldEndGame ~ finalScorePoint:', finalScorePoint);
+  //         console.log('🚀 ~ shouldEndGame ~ progressId:', progressId);
+  //         console.log('🚀 ~ shouldEndGame ~ progressId:', progressId);
+  //         console.log('🚀 ~ shouldEndGame ~ progressId:', progressId);
+  //       }
+  //       console.log('🚀 ~ shouldEndGame ~ progressId:', progressId);
+
+  //       return false;
+  //     };
+
+  //     await shouldEndGame(
+  //       firstPlayerProgress,
+  //       secondPlayerProgress,
+  //       firstPlayerProgress.id,
+  //     );
+  //     await shouldEndGame(
+  //       secondPlayerProgress,
+  //       firstPlayerProgress,
+  //       secondPlayerProgress.id,
+  //     );
+  //   };
+
+  //   await Promise.all(games!.map(processGame));
+  //   return true;
+  // }
 }
